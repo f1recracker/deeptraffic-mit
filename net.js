a9f75e91dabb0434c8ffa76a269a676e5639f193
Harsh Mohan <harshweb@gmail.com>
2018-01-24 20:13:31 -0800

    Added a current best benchmark version (72.5 MPH), tweaked working version

--- net.js
+++ net.js
@@ -2,10 +2,10 @@
 //<![CDATA[
 
 // a few things don't have var in front of them - they update already existing variables the game needs
-lanesSide = 4;
-patchesAhead = 30;
-patchesBehind = 6;
-trainIterations = 100000;
+lanesSide = 5;
+patchesAhead = 40;
+patchesBehind = 10;
+trainIterations = 500000;
 
 // the number of other autonomous vehicles controlled by your network
 otherAgents = 0; // max of 9
@@ -24,6 +24,11 @@ layer_defs.push({
 });
 layer_defs.push({
     type: 'fc',
+    num_neurons: 5 * (lanesSide * 2 + 1),
+    activation: 'relu'
+});
+layer_defs.push({
+    type: 'fc',
     num_neurons: (lanesSide * 2 + 1),
     activation: 'relu'
 });
@@ -33,21 +38,21 @@ layer_defs.push({
 });
 
 var tdtrainer_options = {
-    learning_rate: 0.01,
-    momentum: 0.0,
-    batch_size: 64,
-    l2_decay: 0.05
+    learning_rate: 0.001,
+    momentum: 0.9,
+    batch_size: 128,
+    l1_decay: 0.01
 };
 
 var opt = {};
 opt.temporal_window = temporal_window;
-opt.experience_size = 0.25 * trainIterations;
+opt.experience_size = 0.05 * trainIterations;
 opt.start_learn_threshold = 500;
 opt.gamma = 0.9;
-opt.learning_steps_total = trainIterations;
-opt.learning_steps_burnin = 0.25 * trainIterations;
-opt.epsilon_min = 0.0;
-opt.epsilon_test_time = 0.0;
+opt.learning_steps_total = 0.9 * trainIterations;
+opt.learning_steps_burnin = 0.1 * trainIterations;
+opt.epsilon_min = 0.2;
+opt.epsilon_test_time = 0.1;
 opt.layer_defs = layer_defs;
 opt.tdtrainer_options = tdtrainer_options;
 
@@ -61,6 +66,7 @@ learn = function (state, lastReward) {
     draw_stats();
 
     return action;
+
 }
 
 //]]>
